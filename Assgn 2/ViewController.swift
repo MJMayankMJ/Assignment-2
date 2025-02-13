05c81b92ae6372a30f38d232787a55d5c5ad6d09
Mayank Jangid <226301131@gkv.ac.in>
2025-02-13 16:12:01 +0530

    Initial Commit

---
+++ Assgn 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assgn 2
+//
+//  Created by Mayank Jangid on 2/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Assgn 2Tests/Assgn_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Assgn_2Tests.swift
+//  Assgn 2Tests
+//
+//  Created by Mayank Jangid on 2/13/25.
+//
+
+import Testing
+@testable import Assgn_2
+
+struct Assgn_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
